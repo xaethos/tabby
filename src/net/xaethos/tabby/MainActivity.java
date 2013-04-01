2eee0079ace7f873c1018747a27d695bb109ce1b
Diego García <diego@xaethos.net>
2013-04-01 11:55:33 +0800

    Try network request to API

--- src/net/xaethos/tabby/MainActivity.java
+++ src/net/xaethos/tabby/MainActivity.java
@@ -1,10 +1,17 @@
 package net.xaethos.tabby;
 
-import android.os.Bundle;
+import java.io.IOException;
+
+import net.xaethos.tabby.net.ApiRequest;
 import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +20,43 @@ public class MainActivity extends Activity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+
+        ApiGetRequestTask request = new ApiGetRequestTask();
+        request.execute("/");
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
+
+    // ***** Inner classes
+
+    class ApiGetRequestTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... paths) {
+            try {
+                return ApiRequest.get(paths[0]);
+            } catch (IOException e) {
+                Log.e(TAG, "Error fetching from " + paths[0], e);
+                return "error";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            TextView text = (TextView) MainActivity.this
+                    .findViewById(R.id.text);
+            text.setText(result);
+        }
+
+    }
+
 }
---
+++ src/net/xaethos/tabby/net/ApiRequest.java
@@ -0,0 +1,70 @@
+package net.xaethos.tabby.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.util.Log;
+
+public class ApiRequest {
+    private static final String TAG = "ApiRequest";
+
+    private static final String API_ROOT = "http://haltalk.herokuapp.com";
+
+    public static String get(String path) throws IOException {
+        Log.i(TAG, "Starting GET " + path);
+
+        URL url;
+        String result;
+
+        try {
+            url = new URL(API_ROOT + path);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestProperty("Accept",
+                "application/hal+json, application/json, */*; q=0.01");
+
+        int responseCode = conn.getResponseCode();
+        if (responseCode < 200 || responseCode >= 300) {
+            switch (responseCode) {
+            case 401:
+                result = "Invalid username or password";
+                break;
+
+            default:
+                result = conn.getResponseMessage();
+                break;
+            }
+            Log.e(TAG, "GET " + path + " failed with: " + responseCode);
+            return result;
+        }
+
+        InputStream in = conn.getInputStream();
+
+        try {
+            InputStreamReader is = new InputStreamReader(in);
+            StringBuilder sb = new StringBuilder();
+            BufferedReader br = new BufferedReader(is);
+            String read = br.readLine();
+
+            while (read != null) {
+                sb.append(read);
+                read = br.readLine();
+            }
+
+            result = sb.toString();
+        } finally {
+            in.close();
+        }
+
+        return result;
+    }
+
+}
